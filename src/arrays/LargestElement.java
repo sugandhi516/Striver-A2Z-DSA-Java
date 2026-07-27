f18671a2f2ac193aabd8afbfb97a527c70f820b0
sugandhi516 <bansalsugandhi@gmail.com>
2026-07-28 00:57:40 +0530

    Day 1 - Largest Element

---
+++ src/arrays/LargestElement.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class LargestElement {
+    public static int largest(int[] arr){
+        int max = arr[0];
+        for(int i=0; i<arr.length;i++){
+            if(arr[i]>max){
+                max= arr[i];
+            }
+
+        }
+        return max;
+
+    }
+
+}
+
+
+
+
+
