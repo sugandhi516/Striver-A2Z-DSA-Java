c1c73f256aa22c3e44853c221af4c2c1c89e102f
sugandhi516 <bansalsugandhi@gmail.com>
2026-08-05 18:16:39 +0530

    Array leaders gfg

--- src/arrays/ArrayLeaders.java
+++ src/arrays/ArrayLeaders.java
@@ -1,4 +1,27 @@
 package arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ArrayLeaders {
+    public static ArrayList<Integer> leaders(int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(arr[arr.length-1]);
+        int MaxRight= arr[arr.length-1];
+        for(int i = arr.length -2; i >=0; i--){
+            if(arr[i]>=MaxRight){
+                answer.add(arr[i]);
+                MaxRight= arr[i];
+            }
+        }
+        Collections.reverse(answer);
+        return answer;
+    }
+    public static void main(String[] args) {
+
+        int[] arr = {16, 17, 4, 3, 5, 2};
+
+        System.out.println(leaders(arr));
+    }
 }
+
+
