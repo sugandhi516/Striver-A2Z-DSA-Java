cdefeb727149055b7b433c1aeabbe49f3e850543
sugandhi516 <bansalsugandhi6@gmail.com>
2026-08-21 13:52:17 +0530

    Square root gfg

--- src/BinarySearch1D/SquareROot.java
+++ src/BinarySearch1D/SquareROot.java
@@ -1,4 +1,32 @@
 package BinarySearch1D;
 
 public class SquareROot {
+    public static int Squareroot(int n){
+        int ans = 0;
+        int left=0;
+        int right= n;
+        while(left<=right){
+            int mid =left+(right-left)/2;
+            long square = (long) mid * mid;
+
+            if (square == n) {
+                return mid;
+            }
+            else if (square < n) {
+                ans = mid;
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+
+    }
+    public static void main(String[] args) {
+
+        int n = 11;
+
+        System.out.println(Squareroot(n));
+    }
 }
--- src/Sorting/QuickSort.java
+++ src/Sorting/QuickSort.java
@@ -1,4 +1,56 @@
 package Sorting;
 
 public class QuickSort {
+
+    static void quickSort(int[] arr, int low, int high) {
+
+        if (low < high) {
+
+            int p = partition(arr, low, high);
+
+            quickSort(arr, low, p - 1);
+            quickSort(arr, p + 1, high);
+        }
+    }
+
+    static int partition(int[] arr, int low, int high) {
+
+        int pivot = arr[low];
+
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+
+            if (arr[i] <= pivot) {
+                i++;
+            }
+            else if (arr[j] > pivot) {
+                j--;
+            }
+            else {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // Pivot ko correct position par rakho
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+
+        return j;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {4, 6, 2, 5, 7, 3, 1};
+
+        quickSort(arr, 0, arr.length - 1);
+
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
 }
